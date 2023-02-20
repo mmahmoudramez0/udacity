6f5b7df756eddd1887a405cd77f2283aec52d86c
ramez <ramez@ubuntu.myguest.virtualbox.org>
2023-02-20 09:47:23 +0200

    new change

---
+++ roles/setup/files/index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+var server = http.createServer(function (req, res) {
+res.writeHead(200);
+res.end("Hello world!");
+});
+server.listen(3000);var http = require("http");
+var server = http.createServer(function (req, res) {
+res.writeHead(200);
+res.end("Hello world!");
+});
+server.listen(3000);
